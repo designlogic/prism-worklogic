57ebb57b1cf60b4883382d2ec88af5251a7ac3ee
Tyler <gitkraken@dls0.com>
2022-06-09 13:12:59 -0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemoPageComponent } from './pages/demo-page/demo-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/deom', pathMatch: 'full' },
+  { path: 'demo', component: DemoPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
